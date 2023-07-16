88f26ed78e5704ac758b731fa1f6ff6b572cc9fb
abdul <thiagoabdul10@gmail.com>
2023-07-15 21:55:18 -0300

    Searching images

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import './Photo.css'
 import ModalPhoto from './ModalPhoto'
 
-export default function Photo(){
+export default function Photo({ src }){
     const [modal, setModal] = useState(false)
     const showModal = () => {
         setModal(true)
-        // setTimeout(() => setModal(false), 1000)
     }
+
+    const backgroundImageStyle = {
+        backgroundImage: `url(${src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    } 
+
     return (
         <>
-          <li className='Photo' onClick={showModal}></li>
-          {modal && <ModalPhoto handleOnClick={() => setModal(false)}><Photo></Photo></ModalPhoto>}
+          <li className='Photo' style={backgroundImageStyle}>
+          </li>
+          {modal && <ModalPhoto/>}
         </>
     )
 }
--- src/components/PicturesPanel.jsx
+++ src/components/PicturesPanel.jsx
@@ -1,22 +1,35 @@
 import './PicturesPanel.css'
 import InputPhoto from './InputPhoto';
 import Photo from './Photo';
+import { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-export default function PicturesPanel(){
+const apiUrl = `${import.meta.env.VITE_API_URL}/api/photos`
+
+export default function PicturesPanel(){    
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        caches: 'cache'
+    }
+
+    const [images, setImages] = useState([])
+
+    const createPhoto = (bytes) => `data:image/png;base64,${bytes}`
+
+    useEffect(() => {
+        fetch(apiUrl, options)
+        .then(res => res.json())
+        .then(page => setImages(page.content))
+        .catch(console.log)
+    }, [])
     return (
         <section className="PicturesPanel">
-            <ul>
+            <ul id='photosList'>
                 <InputPhoto></InputPhoto>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
-                <Photo></Photo>
+                { images.map(image => (<Photo key={image.id} src={createPhoto(image.content)}/>))}
             </ul>
-            
         </section>
     )
 }
